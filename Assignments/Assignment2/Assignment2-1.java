cffcc460b829411bd63e2618e34a2afdce34773f
Yafei Sun <sun.yaf@northeastern.edu>
2020-09-16 23:12:12 -0700

    add Assignment2

---
+++ Assignments/Assignment2/Assignment2-1.java
@@ -0,0 +1,104 @@
+/**
+ * The deadline of this assignment is 09/21/2018 23:59 PST.
+ * Please feel free to contact Yafei and Yaqi for any questions.
+ */
+
+class Employee {
+    String name;
+    int age;
+    Gender gender;
+    double salary;// salary per month
+
+    // Constructor. Please set all the data in constructor.
+    public Employee(String name, int age, Gender gender, double salary) {
+        //write your code here
+    }
+
+    // Getter for `name`. Return the current `name` data
+    public String getName() {
+        //write your code here
+    }
+
+    // Setter for `name`. Set `name` data
+    public void setName(String name) {
+        //write your code here
+    }
+}
+
+enum Gender {
+    MALE,
+    FEMALE;
+}
+
+public class Assignment2-1 {
+    // Assignment
+
+    /**
+     * Write a method to calculate the Social Security Tax of an employee and print it.
+     * If the salary is less than or equal to 8900, the Social Security Tax is 6.2% of the salary.
+     * If the salary is more than 8900, the Social Security Tax is 6.2% of 106,800.
+     */
+    public double socialSecurityTax(Employee employee) {
+        //write your code here
+    }
+
+    /**
+     * Write a method to calculate an employee's contribution for insurance coverage and print it.
+     * Amount of deduction is computed as follows:
+     * If the employee is under 35, rate is 3% of salary; if the employee is between 35 and 50(inclusive), rate is 4% of salary;
+     * If the employee is between 50 and 60(exclusive), rate is 5% of salary; If the employee is above 60, rate is 6% of salary.
+     */
+    public double insuranceCoverage(Employee employee) {
+        //write your code here
+    }
+
+    /**
+     * Write a method to sort three employees' salary from low to high, and then print their name in order.
+     * For example, Alice's salary is 1000, John's salary is 500, Jenny's salary is 1200, you should print:
+     * John Alice Jenny
+     */
+    public void sortSalary(Employee e1, Employee e2, Employee e3) {
+        //write your code here
+    }
+
+    /**
+     * Write a method to raise an employee's salary to three times of his/her original salary.
+     * Eg: original salary was 1000/month. After using this method, the salary is 3000/month.
+     * Do not change the input of this method.
+     * Try to add a new method in Employee class: public void raiseSalary(double byPercent)
+     */
+    public void tripleSalary(Employee employee) {
+        //write your code here
+    }
+
+    //Extra credit
+
+    /**
+     * I have written some code below. What I want is to swap two Employee objects.
+     * One is Jenny and one is John. But after running it, I got the result below:
+     * Before: a=Jenny
+     * Before: b=John
+     * After: a=Jenny
+     * After: b=John
+     * There is no change after swap()! Do you know the reason why my swap failed?
+     * Write your understanding of the reason and explain it.
+     */
+    /*
+     write your understanding here.
+    */
+    public static void main(String[] args) {
+        Employee a = new Employee("Jenny", 20, Gender.FEMALE, 2000);
+        Employee b = new Employee("John", 30, Gender.MALE, 2500);
+        System.out.println("Before: a=" + a.getName());
+        System.out.println("Before: b=" + b.getName());
+        swap(a, b);
+        System.out.println("After: a=" + a.getName());
+        System.out.println("After: b=" + b.getName());
+    }
+
+    public static void swap(Employee x, Employee y) {
+        Employee temp = x;
+        x = y;
+        y = temp;
+    }
+}
---
+++ Assignments/Assignment2/Assignment2-2.java
@@ -0,0 +1,22 @@
+public class Assignment2-1 {
+        /**
+         * Given an integer array nums, find the contiguous subarray (containing at least one number)
+         * which has the largest sum and outprint its sum.
+         *
+         * Example 1:
+         * Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+         * Output: 6
+         * Explanation: [4,-1,2,1] has the largest sum = 6.
+         *
+         * Example 2:
+         * Input: nums = [1]
+         * Output: 1
+         */
+        public int maxSubArray(int[] nums) {
+            //write your code here
+        }
+        public static void main(String[] args) {
+            //write your code here
+        }
+
+}
