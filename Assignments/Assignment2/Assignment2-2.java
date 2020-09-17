29ec201746c22541602128cb2f68189740ef9195
Yafei Sun <sun.yaf@northeastern.edu>
2020-09-16 23:19:50 -0700

    Update Assignment2

--- Assignments/Assignment2/Assignment2-2.java
+++ Assignments/Assignment2/Assignment2-2.java
@@ -1,22 +1,27 @@
-public class Assignment2-1 {
-        /**
-         * Given an integer array nums, find the contiguous subarray (containing at least one number)
-         * which has the largest sum and outprint its sum.
-         *
-         * Example 1:
-         * Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-         * Output: 6
-         * Explanation: [4,-1,2,1] has the largest sum = 6.
-         *
-         * Example 2:
-         * Input: nums = [1]
-         * Output: 1
-         */
-        public int maxSubArray(int[] nums) {
-            //write your code here
-        }
-        public static void main(String[] args) {
-            //write your code here
-        }
+/**
+ * The deadline of this assignment is 09/21/2018 23:59 PST.
+ * Please feel free to contact Yafei and Yaqi for any questions.
+ */
 
+public class Assignment2-2 {
+    /**
+     * Given an integer array nums, find the contiguous subarray (containing at least one number)
+     * which has the largest sum and outprint its sum.
+     *
+     * Example 1:
+     * Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+     * Output: 6
+     * Explanation: [4,-1,2,1] has the largest sum = 6.
+     *
+     * Example 2:
+     * Input: nums = [1]
+     * Output: 1
+     */
+    public double socialSecurityTax(Employee employee) {
+        //write your code here
+    }
+
+    public static void main(String[] args) {
+        //write your code here
+    }
 }
