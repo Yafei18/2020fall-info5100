2987e566db965d364b55277a1760c77e81d4a45a
Siva Dosapati <Siva@namrix.com>
2020-12-01 17:17:29 -0800

    Changes

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -4,6 +4,10 @@ public class FirstProgram {
 	public static void main(String[] args) {
 		System.out.println("Welcome to NEU!");
 		System.out.println("Welcome again");
+		
+		System.out.println("God is great");
+		
+		System.out.println("XYZ");
 	}
 
 }
